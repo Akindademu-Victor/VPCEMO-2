411187fc89a3cd89751993a2072334976e931e88
Akindademu-Victor <funsho555@gmail.com>
2022-10-16 17:31:45 -0400

    update terraform files

--- apache-install.sh
+++ apache-install.sh
@@ -14,3 +14,11 @@ sudo chkconfig docker on
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -a)-0 /usr/local/bin/docker-commpose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo reboot
+sudo yum install wget
+sudo amazon-linux-extras install java-openjdk11
+sudo amazon-linux-extras install epel -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum install jenkins -y
+sudo service jenkins enable 
+sudo service jenkins start
